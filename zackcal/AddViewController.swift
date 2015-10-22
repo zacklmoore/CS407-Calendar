7714055a5d1f5f30d91c4ecadc6e1c3ba2d91f86
Zackary Moore <zacklmoore@gmail.com>
2015-10-21 21:48:36 -0500

    Created initial Swift files for View Controllers and necessary classes. Setup basic storyboard layout.

---
+++ zackcal/AddViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AddViewController.swift
+//  zackcal
+//
+//  Created by Zackary Moore on 10/21/15.
+//  Copyright (c) 2015 Zackary Moore. All rights reserved.
+//
+
+import Foundation
---
+++ zackcal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  zackcal
+//
+//  Created by Zackary Moore on 10/11/15.
+//  Copyright (c) 2015 Zackary Moore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
